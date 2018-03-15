02323d5ca5672af69b469d5f9c49a21fa6145f3b
Nikhil CSB <csbnikhil@gmail.com>
2018-03-15 12:01:51 +0530

    Added main

---
+++ main/java/hostel/fledged/OTP.java
@@ -0,0 +1,77 @@
+package hostel.fledged;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class OTP extends AppCompatActivity {
+    EditText otpText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+
+
+        Bundle data = getIntent().getExtras();
+        final String otp = data.getString("otp");
+        final String phone = data.getString("phone");
+        final String usn = data.getString("usn");
+
+        WebView sendURL = findViewById(R.id.webview);
+
+        final Button verifyBtn = findViewById(R.id.button2);
+
+        String url = "http://192.168.1.35/?phone="+phone+"&otp="+otp;
+
+        Toast.makeText(getApplicationContext(), otp, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getApplicationContext(), phone, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getApplicationContext(), usn, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Sending OTP via "+url, Toast.LENGTH_LONG).show();
+
+        sendURL.loadUrl(url);
+
+        verifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                otpText = findViewById(R.id.otp);
+                if (otpText.getText().toString().equals(otp)) {
+                    Toast.makeText(getApplicationContext(), "Generating QR...", Toast.LENGTH_SHORT).show();
+                    try {
+                        generateQRCode();
+                    } catch (Exception e) {
+                        Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                else
+                    Toast.makeText(getApplicationContext(), "Incorrect", Toast.LENGTH_SHORT).show();
+            }
+
+            private void generateQRCode() {
+
+                Toast.makeText(getApplicationContext(), "Starting QRCode.class...", Toast.LENGTH_SHORT).show();
+
+                //Data to be sent ot the next activity
+                Bundle data = new Bundle();
+
+                data.putString("usn", usn);
+                data.putString("phone", phone);
+
+
+                Toast.makeText(getApplicationContext(), "Starting QRCode.class...", Toast.LENGTH_SHORT).show();
+
+                // Start QRCode.class
+                Intent generate = new Intent(OTP.this, QRCode.class);
+
+                generate.putExtras(data);
+                startActivity(generate);
+            }
+        });
+    }
+}
---
+++ main/java/hostel/fledged/Verification.java
@@ -0,0 +1,61 @@
+package hostel.fledged;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.io.IOException;
+
+public class Verification extends AppCompatActivity {
+
+    EditText mUSN, mPhone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+
+        final Button sendVerificationBtn = findViewById(R.id.button);
+        mUSN = findViewById(R.id.usn);
+        mPhone = findViewById(R.id.phone);
+
+        sendVerificationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    sendVerificationNumber();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), e.toString(),
+                            Toast.LENGTH_SHORT).show();                }
+            }
+
+            private void sendVerificationNumber() throws IOException {
+                String usn = mUSN.getText().toString(), phone = mPhone.getText().toString(), otp = getRandom();
+                Bundle otpData = new Bundle();
+
+                // Bundling data to be transferred...
+
+                otpData.putString("phone", phone);
+                otpData.putString("otp", otp);
+                otpData.putString("usn", usn);
+
+                Intent i = new Intent(Verification.this, OTP.class);
+
+                i.putExtras(otpData);
+
+                startActivity(i);
+            }
+        });
+
+    }
+
+    private static String getRandom(){
+        int n = (int) (Math.random()*1000000);
+        return Integer.toString(n);
+    }
+}
